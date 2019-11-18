36cf25d02cef05052c41d84ba2a67748894fbe6b
Nitin Chaurasia <gs.nitin@gmail.com>
2019-11-18 01:29:00 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # netflix-eureka-naming-server
+
+### Spring Initializer
+* Eureka Server
+* Config Client
+* Spring Boot Actuator
+* Spring Boot DevTools
---
+++ src/test/java/com/nitin/microservices/learning/eurekaNamingServier/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.microservices.learning.eurekaNamingServier.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
